d7354c22bfac7746e575b01238a3aae29b3a6570
Nick <nickwentworth123@gmail.com>
2026-05-21 22:01:37 -0400

    feat(web): integrate react-hook-form into txn forn

--- web/src/components/TransactionForm.tsx
+++ web/src/components/TransactionForm.tsx
@@ -1,18 +1,67 @@
-import { useState } from 'react';
 import { Button } from './Button';
 import { Icon } from './Icon';
 import { Text } from './Text';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+
+const TXN_RECUR_PERIODS = ['day', 'week', 'month', 'year'] as const;
+
+const txnRecurSchema = z.object({
+    rate: z.coerce.number(),
+    period: z.enum(TXN_RECUR_PERIODS),
+    daysOfWeek: z.number().array().optional(),
+    daysOfMonth: z.number().array().optional(),
+    daysOfYear: z.number().array().optional(),
+    endsAt: z.preprocess(
+        (val) => (val === '' ? undefined : val),
+        z.coerce.date().optional(),
+    ),
+});
+
+const txnFormSchema = z.object({
+    isExpense: z.boolean(),
+    amount: z.coerce.number(),
+    categoryId: z.coerce.number(),
+    date: z.coerce.date(),
+    description: z.string().optional(),
+    isRecurring: z.boolean(),
+    recurrence: txnRecurSchema.optional(),
+});
+
+type TxnFormData = z.infer<typeof txnFormSchema>;
 
 type TransactionFormProps = {
     onCloseClick: () => void;
 };
 
 export function TransactionForm(props: TransactionFormProps) {
-    const [isExpense, setIsExpense] = useState(true);
-    const [isRecurring, setIsRecurring] = useState(false);
+    const { register, watch, setValue, handleSubmit } = useForm<TxnFormData>({
+        defaultValues: {
+            isExpense: true,
+            date: new Date(), // FIXME: not currently working
+            isRecurring: false,
+            recurrence: {
+                // rate: 1,
+                period: 'week',
+            },
+        },
+    });
+
+    const isExpense = watch('isExpense');
+    const isRecurring = watch('isRecurring');
+
+    const onSubmit = handleSubmit((data: any) => {
+        console.log(data);
+        const parsed = txnFormSchema.parse({
+            ...data,
+            recurrence: data.isRecurring ? data.recurrence : undefined,
+        });
+        console.log(parsed);
+        // TODO: send to api
+    });
 
     return (
-        <div className='flex flex-col gap-4 p-6'>
+        <form className='flex flex-col gap-4 p-6' onSubmit={onSubmit}>
             <div className='flex justify-between'>
                 <h2>Add Transaction</h2>
                 <Button variant='ghost' onClick={props.onCloseClick}>
@@ -23,23 +72,27 @@ export function TransactionForm(props: TransactionFormProps) {
             <div className='border-cream-400 border text-3xl font-mono font-medium rounded-lg overflow-hidden flex'>
                 <button
                     className='w-12 bg-cream-200 border-cream-400 border-r shrink-0'
-                    onClick={() => setIsExpense((b) => !b)}
+                    onClick={() => setValue('isExpense', !isExpense)}
                 >
-                    {isExpense ? '+' : <>&ndash;</>}
+                    {isExpense ? <>&ndash;</> : '+'}
                 </button>
                 <span className='pl-2 pr-1 py-3'>$</span>
                 <input
                     className='min-w-0 grow pr-2'
                     type='text'
                     placeholder='0.00'
+                    {...register('amount', { required: true })}
                 />
             </div>
 
             <div className='grid grid-cols-2 gap-4'>
                 <label className='flex flex-col gap-2'>
                     <Text variant='uppercase'>Category</Text>
-                    <select className='border-cream-400 border rounded-lg p-2 font-semibold'>
-                        <option>Salary</option>
+                    <select
+                        className='border-cream-400 border rounded-lg p-2 font-semibold'
+                        {...register('categoryId', { required: true })}
+                    >
+                        <option value={1}>Salary</option>
                     </select>
                 </label>
 
@@ -48,6 +101,7 @@ export function TransactionForm(props: TransactionFormProps) {
                     <input
                         className='border-cream-400 border rounded-lg p-2 font-semibold'
                         type='date'
+                        {...register('date', { required: true })}
                     />
                 </label>
             </div>
@@ -58,6 +112,7 @@ export function TransactionForm(props: TransactionFormProps) {
                     className='border-cream-400 border rounded-lg p-2 resize-y'
                     placeholder='Add a note...'
                     rows={3}
+                    {...register('description')}
                 ></textarea>
             </label>
 
@@ -67,7 +122,9 @@ export function TransactionForm(props: TransactionFormProps) {
                     <input
                         type='checkbox'
                         checked={isRecurring}
-                        onChange={(e) => setIsRecurring(e.target.checked)}
+                        onChange={(e) =>
+                            setValue('isRecurring', e.target.checked)
+                        }
                     />
                     Off
                 </div>
@@ -81,9 +138,19 @@ export function TransactionForm(props: TransactionFormProps) {
                             <input
                                 className='bg-cream-50 border-cream-400 border rounded-lg p-2 font-semibold w-16'
                                 type='number'
+                                {...register('recurrence.rate', {
+                                    required: true,
+                                })}
                             />
-                            <select className='bg-cream-50 border-cream-400 border rounded-lg p-2 font-semibold grow'>
-                                <option>week</option>
+                            <select
+                                className='bg-cream-50 border-cream-400 border rounded-lg p-2 font-semibold grow'
+                                {...register('recurrence.period')}
+                            >
+                                {TXN_RECUR_PERIODS.map((period) => (
+                                    <option value={period} key={period}>
+                                        {period}
+                                    </option>
+                                ))}
                             </select>
                         </div>
                     </label>
@@ -92,6 +159,7 @@ export function TransactionForm(props: TransactionFormProps) {
                         <Text variant='uppercase'>Repeat on</Text>
                         <div className='flex gap-2'>
                             {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day) => (
+                                // TODO: days of week/month/year, depending on recurrence.period
                                 <button className='bg-cream-50 border-cream-400 border w-9 h-9 rounded-full font-semibold'>
                                     {day}
                                 </button>
@@ -104,6 +172,7 @@ export function TransactionForm(props: TransactionFormProps) {
                         <input
                             className='bg-cream-50 border-cream-400 border rounded-lg p-2 font-semibold'
                             type='date'
+                            {...register('recurrence.endsAt')}
                         />
                     </label>
                 </div>
@@ -117,6 +186,6 @@ export function TransactionForm(props: TransactionFormProps) {
                     Save
                 </Button>
             </div>
-        </div>
+        </form>
     );
 }
